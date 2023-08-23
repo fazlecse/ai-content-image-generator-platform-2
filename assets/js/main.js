2d745873ce5ce9c222dd14809feb9012a5041e31
Md. Fazle Rabbi <mdfazlerabbi06@gmail.com>
2023-08-23 20:12:16 +0600

    23-08-2023
    pricing plan, testimonial, faq, blog

--- assets/js/main.js
+++ assets/js/main.js
@@ -41,103 +41,34 @@ $(document).ready(function () {
         }
     });
 
-    // What's New carousel
-    $('.carousel-area2').owlCarousel({
-        loop: true,
-        autoplay: false,
-        margin: 20,
-        nav: true,
-        dots: false,
-        // rtl:true,
-        responsive: {
-            0: {
-                items: 1
-            },
-            575: {
-                items: 3
-            },
-            1000: {
-                items: 4
-            },
-            1400: {
-                items: 5
-            },
-            1620: {
-                items: 6
-            },
-        }
-    });
-    // Stream channel What's New carousel
-    $('.carousel-area3').owlCarousel({
-        loop: true,
-        autoplay: false,
-        margin: 20,
-        nav: true,
-        dots: false,
-        // rtl:true,
-        responsive: {
-            0: {
-                items: 1
-            },
-            400: {
-                items: 2
-            },
-            600: {
-                items: 3
-            },
-            800: {
-                items: 4
-            },
-            1000: {
-                items: 4
+
+    // Testimonial section start
+    // Owl carousel 
+    $(function (e) {
+        "use strict";
+        $('.testimonial-carousel').owlCarousel({
+            loop: true,
+            autoplay: true,
+            center: true,
+            margin: 10,
+            nav: false,
+            dots: true,
+
+            // rtl: true,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                600: {
+                    items: 2
+                },
+                1000: {
+                    items: 3
+                }
             }
-        }
+        });
     });
-    // responsive-filter
-    $('.carousel-area4').owlCarousel({
-        loop: true,
-        autoplay: false,
-        margin: 20,
-        nav: true,
-        dots: false,
-        // rtl:true,
-        responsive: {
-            0: {
-                items: 1
-            },
-            420: {
-                items: 2
-            },
-            550: {
-                items: 3
-            },
-            720: {
-                items: 4
-            },
-        }
-    });
-
-    // Swiper-slider livestream-swiper
-    var swiper = new Swiper(".livestream-swiper", {
-        effect: "coverflow",
-        grabCursor: true,
-        centeredSlides: true,
-        slidesPerView: "auto",
-        coverflowEffect: {
-            rotate: 0,
-            stretch: 0,
-            depth: 100,
-            modifier: 1,
-            slideShadows: true,
-        },
-        // Navigation arrows
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
-        loop: true,
-
-    });
+    // Testimonial section end
 
     // Qr section start
     var qr = QRCode.generatePNG(document.getElementById("qrUrl").value, {
